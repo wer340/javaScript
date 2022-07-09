061a1c0de9cfda6aa2c88316dc7b6a3041618ede
hamed.sh <hsee1372@gmail.com>
2022-07-09 16:36:50 +0430

    initial jquery

---
+++ jQuery/frustratingCode.js
@@ -0,0 +1,4 @@
+for (var i=0; i<document.querySelectorAll("p").length;i++){
+    document.querySelectorAll("button").addEventListener("click",function(e){
+document.querySelector("h1").style.color="red"
+    }}

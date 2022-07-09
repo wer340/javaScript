d153e32bf2858b52345aff143c56e279d9b10e5f
hamed.sh <hsee1372@gmail.com>
2022-07-09 17:09:52 +0430

    addClass hasClass give many class as parameter

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,5 +1,7 @@
-$(document).ready(function(){
-    $("h1").css("color","red")
-}
 
-)
+    $("h1").css("color","red")
+    $("h1").addClass("title-js color") // seprate behavior css js html  
+
+console.log($("h1").css("fontSize")) // expect value of property
+console.log($("h1").hasClass("color")) // expect value of property
+

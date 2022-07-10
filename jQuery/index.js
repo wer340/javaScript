e1406d5017a4a8a64d803273e49f613ba5ee7266
hamed.sh <hsee1372@gmail.com>
2022-07-10 18:23:11 +0430

    set and get attribute by jquery

--- jQuery/index.js
+++ jQuery/index.js
@@ -7,4 +7,5 @@ console.log($("h1").hasClass("color")) // expect value of property
 $("button").text("Click Scarlett") // select all button change all text thats selected
 $(".photo").html('<img src="asset/3.jpg" alt="">'); //add photo 
 $($("button")[1]).html("<em>scarlett Johansson</em>") // select single element of them 
-
+console.log($("img").attr("src")); //property show  ~  getAttribute 
+$("img").attr("height","200px")  // set attribute this way wrong  because seprate behavior css js html

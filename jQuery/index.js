a22b32504455912f90ada14f30fd4b9cf7aec726
hamed.sh <hsee1372@gmail.com>
2022-07-10 18:14:17 +0430

    manipulate .text ().html()

--- jQuery/index.js
+++ jQuery/index.js
@@ -4,4 +4,7 @@
 
 console.log($("h1").css("fontSize")) // expect value of property
 console.log($("h1").hasClass("color")) // expect value of property
+$("button").text("Click Scarlett") // select all button change all text thats selected
+$(".photo").html('<img src="asset/3.jpg" alt="">'); //add photo 
+$($("button")[1]).html("<em>scarlett Johansson</em>") // select single element of them 
 

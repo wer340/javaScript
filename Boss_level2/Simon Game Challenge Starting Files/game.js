70cefde5f9fe9b29813bb8cb7ae7b5e92163aee1
hamed.sh <hsee1372@gmail.com>
2022-07-12 12:19:32 +0430

    level 0  and sound

--- Boss_level2/Simon Game Challenge Starting Files/game.js
+++ Boss_level2/Simon Game Challenge Starting Files/game.js
@@ -1,12 +1,49 @@
 var gamePattern=[];
+var userClickedPattern=[];
 var buttonColours=["red", "blue", "green", "yellow"] ;
 function nextSequence(){
     var randomNumber=Math.random()*4;
     randomNumber=Math.floor(randomNumber);
-    return randomNumber
-}
-var randomChosenColour =buttonColours[nextSequence()];
+    var randomChosenColour =buttonColours[randomNumber];
 gamePattern.push(randomChosenColour);
-console.log(gamePattern)
-
 $("#"+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    
+}
+
+// console.log("gamePattern="+gamePattern)
+
+
+$(".btn").click(function(){
+    // console.log(this.id)
+    if(this.id==gamePattern[0]){
+        var audio = new Audio('sounds/'+this.id+'.mp3');
+        audio.play();
+    }else{
+        var audio = new Audio('sounds/wrong.mp3');
+        audio.play();
+    }
+    
+
+})
+
+    $(".btn").click(function(){
+        var userChosenColour =this.id;
+        userClickedPattern.push(userChosenColour);
+        console.log("userPattern="+userClickedPattern)
+    })
+    $(".btn").click(function(){
+            
+            $(this).addClass("pressed");
+      setTimeout(function(){
+        $(".pressed").removeClass("pressed")
+
+      },100)
+
+    });
+    $(document).keypress(function(e){
+        if(e.key=="a"){
+            nextSequence();
+            $("#level-title").text("Level 0")
+        }
+        
+    });

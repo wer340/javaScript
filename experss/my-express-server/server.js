4474945d7829058826468e10d3942def4b938bb2
hamed.sh <hsee1372@gmail.com>
2022-07-13 11:33:42 +0430

    issue path hardware

--- experss/my-express-server/server.js
+++ experss/my-express-server/server.js
@@ -1,8 +1,13 @@
 const express=require("express");
 
 const app= express();
-app.get("/",(request,response)=>{
-   response.send("hello")
+app.get("/",(request,response)=>{ //response=res  request=req
+   response.send(`
+   <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Vivien_Leigh_Scarlet.jpg/220px-Vivien_Leigh_Scarlet.jpg"  >
+   
+   <img src=".\vivien.jpg" >
+   
+   `)
 })
 app.listen(port=3000,()=>{
     console.log("server started 3000")

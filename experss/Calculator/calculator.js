3bfa80f6234bb39705c97ce0c0a1d997d2e634c2
hamed.sh <hsee1372@gmail.com>
2022-07-13 12:25:55 +0430

    calculator

---
+++ experss/Calculator/calculator.js
@@ -0,0 +1,16 @@
+const express=require("express");
+
+
+
+const app=express();
+
+app.get("/",function(req,res){
+res.send("hi")
+})
+
+
+
+
+app.listen(3000,function(){
+    console.log("server started 3000");
+})
--- experss/my-express-server/server.js
+++ experss/my-express-server/server.js
@@ -3,6 +3,7 @@ const express=require("express");
 const app= express();
 app.get("/",(request,response)=>{ //response=res  request=req
    response.send(`
+   <h1>hello Scarlett gone with wind</h1>
    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Vivien_Leigh_Scarlet.jpg/220px-Vivien_Leigh_Scarlet.jpg"  >
    
    <img src=".\vivien.jpg" >

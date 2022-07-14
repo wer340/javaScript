8da40368887317407b0dd29395e00d5d947732d5
hamed.sh <hsee1372@gmail.com>
2022-07-14 10:08:21 +0430

    static adress

--- experss/my-express-server/public/app-photo.js
+++ experss/my-express-server/public/app-photo.js
@@ -1,7 +1,7 @@
 var name=['Lana Turner', 'Janet Leigh ', 'Keira Knightley', 'Jane Seymour', 'Natalie Portman', 'Grace Kelly', 'Penelope Cruz', 'Elizabeth Taylor', 'Winona Ryder', 'Audrey Hepburn', 'Monica Bellucci']
 
 
-
+alert("")
 // for (item of name){
 //     document.querySelector(".wrapper").innerHTML+=`
 //     <div class="item"></div>
--- experss/my-express-server/server.js
+++ experss/my-express-server/server.js
@@ -1,12 +1,13 @@
 const express=require("express");
 
 const app= express();
+app.use(express.static('public'))
 app.get("/",(request,response)=>{ //response=res  request=req
    response.send(`
    <h1>hello Scarlett gone with wind</h1>
    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Vivien_Leigh_Scarlet.jpg/220px-Vivien_Leigh_Scarlet.jpg"  >
    
-   "<img src=".\vivien.jpg" > " this method for sned html not file
+   "<img src=".\vivien.jpg" > " this method for send html not file
    
    `);
    response.sendFile(__dirname+"/vivien.jpg")
@@ -15,7 +16,7 @@ app.get("/women",function(req,res){
     res.sendFile(__dirname+"/vivien.jpg")
 });
 app.get("/photo",function(req,res){
-    res.sendFile(__dirname+"/photo.html")
+    res.sendFile(__dirname+"/public/photo.html")
 });
 
 app.listen(port=3000,()=>{

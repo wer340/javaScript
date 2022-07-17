a930a26960b6b815e8d9db15a0a2b816f47b7926
hamed.sh <hsee1372@gmail.com>
2022-07-17 16:09:24 +0430

    post cat

--- API/joke-page/app.js
+++ API/joke-page/app.js
@@ -1,31 +1,48 @@
 const express=require("express");
+const bodyParser=require("body-parser")
 const https=require("https"); // native module of node
 // const bodyParser=require("body-parser");
 const app=express();
+const flagList=["Programming","Miscellaneous","Dark","Pun","Spooky","Christmas"]
+var isList=0;
+app.use(bodyParser.urlencoded({extended:true}));
 
+    
+    
+    
+    
+    
+    
 
 app.get("/",(req,res)=>{
-    var  url="https://v2.jokeapi.dev/joke/programming"
-    https.get(url,function(response){
-       console.log(response.statusCode);
 
-       response.on("data",function(data){
-        const objectJoke=JSON.parse(data);
-       res.write("<h1> stateCode : "+response.statusCode+"</h1>");
-       res.write("<h1>"+objectJoke.category+"</h1>");
-       res.write("<h4>"+objectJoke.joke+"</h4>");
-       res.write("<h1><b>"+objectJoke.error+"</b></h1>");
-       res.send()
-    });
-
-
-
-    });
-
-
+res.sendFile(__dirname+"/joke.html")
 });
 
-
+app.post("/",function(req,res){
+ flagList.forEach((item)=>{item==req.body.cat ?  isList+=1 : isList+=0 });
+        var  url="https://v2.jokeapi.dev/joke/"+req.body.cat;
+        https.get(url,function(response){
+           console.log(response.statusCode);//bunch
+        
+           response.on("data",function(data){
+            const objectJoke=JSON.parse(data);
+           res.write("<h1> stateCode : "+response.statusCode+"</h1>");
+           res.write("<h1> category :"+objectJoke.category+"</h1>");
+           res.write("<p>joke : "+objectJoke.joke+"</p>");
+           res.write("<h1>error : <b>"+objectJoke.error+"</b></h1>");
+           res.write("<h1>isList : <b>"+isList+"</b></h1>");
+           res.send()
+        });
+        
+        });
+        
+  
+        
+       
+       
+    
+})
 
 
 

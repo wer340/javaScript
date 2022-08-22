4018b1b900c2bdb2b279fbf61272ce5a4d4f68f2
hamed.sh <hsee1372@gmail.com>
2022-08-22 18:32:11 +0430

    validation complete

--- mongoose/index.js
+++ mongoose/index.js
@@ -6,7 +6,7 @@ console.log("ssssss")
 
 const movieSchema=new mongoose.Schema({
     name:String,
-    year:Number
+    year:{type:Number ,required:[true,"please correct and full year"]}
     
 })
 const personSchema=new mongoose.Schema({
@@ -16,24 +16,50 @@ const personSchema=new mongoose.Schema({
 })
 const Movie=mongoose.model("movie",movieSchema)
 const Person=mongoose.model("person",personSchema)
-async function insertDate(data){
+// !insert data function
+async function insertData(data){
 const movie=new Movie(data)
 await movie.save()
-
+    
 }
 
+// !insert many data function
 async function personInsert(data){
 listModelData=[]    
 for(item of data){
     let person=new Person(item)
     listModelData.push(person)
 }
-Person.insertMany(listModelData)
+Person.insertMany(listModelData,(err)=>{
+if(err){
+    console.log(err)
+}else{
+    console.log("data successfully stored in database")
+}
+})
 }
 
-personInsert([
+/*personInsert([
     {name:"Scarlett",age:45,movie:"Avenger"},
     {name:"emilia",age:35,movie:"Game of Thrones"}
     
-])
-db.people.deleteOne({"_id":ObjectId("630237c12a072787c87d3d63")})
+])*/
+//db.people.deleteOne({"_id":ObjectId("630237c12a072787c87d3d63")})
+
+// !read
+
+async function readDb(Data){
+    Data.find((err,people)=>{
+if(err){
+    console.log("error : ",err)
+}else{
+    mongoose.connection.close()
+    for(item of people){
+    console.log("name : =>",item.name)    
+        
+    }
+}
+})
+}
+insertData({name:"apartment"})
+readDb(Movie)
